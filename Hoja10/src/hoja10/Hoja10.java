41dadc03994c155abf5d1bc0ac86e146e8fd409d
dbs_jd <dbs_jd@172.20.7.210>
2015-11-04 08:07:04 -0600

    Listo todo. 150/100

---
+++ Hoja10/src/hoja10/Hoja10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja10;
+
+/**
+ *
+ * @author dbs_jd
+ */
+public class Hoja10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("la ruta mas corta es: la integral de línea desde t=0 hasta t=3, parametrizando x=cost, y = sent, z = 1");
+        // TODO code application logic here
+    }
+    
+}
