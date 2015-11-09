ae428352d9086c504b69c525aa6344d5df56a688
ben14124 <ben14124@uvg.edu.gt>
2015-11-09 13:32:28 -0600

    Commit

--- Hoja10/src/hoja10/Hoja10.java
+++ Hoja10/src/hoja10/Hoja10.java
@@ -31,7 +31,31 @@ public class Hoja10 {
         {
             graph.setData(i, i);
         }
-
+        int[][] matriz1 = new int[3][3];
+        int[][] matriz2 = new int[3][3];
+        int[][] matriz3 = new int[3][3];
+        
+        matriz3[0][0] = 0;
+        matriz3[0][1] = 1;
+        matriz3[0][2] = 1;
+        matriz3[0][3] = 0;
+        matriz3[1][0] = 0;
+        matriz3[1][1] = 0;
+        matriz3[1][2] = 0;
+        matriz3[1][3] = 1;
+        matriz3[2][0] = 0;
+        matriz3[2][1] = 1;
+        matriz3[2][2] = 0;
+        matriz3[2][3] = 1;
+        matriz3[3][0] = 0;
+        matriz3[3][1] = 0;
+        matriz3[3][2] = 0;
+        matriz3[3][3] = 0;
+        
+        Floyd algoritmo1 = new Floyd();
+        
+        algoritmo1.Floyd(4, matriz3, matriz1, matriz2);
+        
         // now add the links between digraph nodes
 
         graph.addLinks(1,Arrays.asList(2,3));
